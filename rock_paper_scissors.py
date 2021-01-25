8429b38dba824b0b856a5515dd7b5ef68d85a76f
John Cokins <il7148pg@go.minnstate.edu>
2021-01-24 22:38:08 -0600

    removed verbose code in the function that determined the winner. Cleaned up, checked everything over. Fixed bug where scores weren't increasing. Added a random element in the computer's moves, there's a 20% chance any given computer move (after the first) is random. Fun project. JC

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -23,16 +23,30 @@ def main():
     # number of wins computer has
     computer_score = 0
     # explanation of rules
-    print('For each round please make a choice between rock, paper, or scissors.\nTo play rock type \"1\"'
-          '\nTo play paper type \"2\"\nTo play scissors type \"3\"')
+    print('For each round please make a choice between rock, paper, or scissors.\nThe program '
+          'will make a choice and keep it secret before the user chooses\nTo play rock type \"1\"'
+          '\nTo play paper type \"2\"\nTo play scissors type \"3\"\n')
     while current_round < number_of_rounds:
         computer_move = determine_computer_move(last_user_move, last_computer_move, computer_won_last_round)
         user_move = determine_user_move(current_round, computer_score, user_score)
+        print(f'Computer has chosen {possible_moves[(computer_move - 1)]}\n')
         last_user_move = user_move
         last_computer_move = computer_move
-        determine_outcome(user_move, computer_move, current_round, computer_score, user_score)
-
-    print(f'Final scores:\nComputer: {computer_score} wins\nUser: {user_score} wins')
+        if computer_move != user_move:
+            does_computer_win = determine_outcome(user_move, computer_move)
+            current_round += 1
+            if does_computer_win:
+                computer_score += 1
+            else:
+                user_score += 1
+            computer_won_last_round = does_computer_win
+        else:
+            print(f'User and computer have both played {user_move}, redo round')
+    print(f'Final scores:\nComputer wins: {computer_score}\nUser wins: {user_score}\n')
+    if computer_score > user_score:
+        print('Computer wins! Fun game!')
+    else:
+        print('User wins, nice job!')
 
 def determine_number_rounds():
     try:
@@ -50,7 +64,7 @@ def determine_number_rounds():
 def determine_user_move(current_round, computer_score, user_score):
 
     # ask user for input, rock paper or scissors
-    user_move = int(input(f'Round {(current_round + 1)}:\n'
+    user_move = int(input(f'Round {(current_round + 1)}------------------------------------:\n'
                           f'Current score is computer {computer_score} player: {user_score}\n'
                           f'Make your move (1 = rock, 2 = paper, or 3 = scissors): '))
     while type(user_move) != int or user_move < 1 or user_move > 3:
@@ -65,8 +79,14 @@ def determine_computer_move(last_user_move, last_computer_move, computer_won_las
     # determine which strategy to use
 
     # has there been a match? if yes we will use the data to predict the next move
+    # there's a 1 in 5 chance its random anyway, to hopefully stop someone figuring out our strategy
+    chaos_factor = [0, 0, 0, 0, 1]
+    hot_potato = int(random.choice(chaos_factor))
 
-    if last_user_move and computer_won_last_round:
+    if hot_potato:
+        computer_move = random.randint(1, 3)
+
+    elif last_user_move and computer_won_last_round:
         # strategy 1, case user just lost, choose user's last move
         computer_move = last_user_move
 
@@ -83,38 +103,18 @@ def determine_computer_move(last_user_move, last_computer_move, computer_won_las
 
 
 # METHOD to determine who won
-def determine_outcome(user_move, computer_move, current_round, computer_score, user_score):
-    if user_move == computer_move:  # user and computer tie
-        print('tie, replay round')
-    elif user_move == 1:  # user selected rock
-        if computer_move == 2:  # computer selected paper
-            print(f'Computer wins with {possible_moves[(computer_move - 1)]} beating {possible_moves[(user_move - 1)]}')
-            current_round += 1
-            computer_score += 1
-        elif computer_move == 3:  # computer selected scissors
-            print(f'User wins with {possible_moves[(user_move - 1)]} beating {possible_moves[(computer_move - 1)]}')
-            current_round += 1
-            user_score += 1
-
-    elif user_move == 2:  # user selected paper
-        if computer_move == 3:  # computer selected scissors
-            print(f'Computer wins with {possible_moves[(computer_move - 1)]} beating {possible_moves[(user_move - 1)]}')
-            current_round += 1
-            computer_score += 1
-        elif computer_move == 1:  # computer selected rock
-            print(f'User wins with {possible_moves[(user_move - 1)]} beating {possible_moves[(computer_move - 1)]}')
-            current_round += 1
-            user_score += 1
-
-    elif user_move == 3:  # user selected scissors
-        if computer_move == 1:  # computer selected rock
-            print(f'Computer wins with {possible_moves[(computer_move - 1)]} beating {possible_moves[(user_move - 1)]}')
-            current_round += 1
-            computer_score += 1
-        elif computer_move == 2:  # computer selected paper
-            print(f'User wins with {possible_moves[(user_move - 1)]} beating {possible_moves[(computer_move - 1)]}')
-            current_round += 1
-            user_score += 1
+# if computer wins outcome is true, else it is false
+def determine_outcome(user_move, computer_move):
+    if (user_move == 1 and computer_move == 2) \
+            or (user_move == 2 and computer_move == 3) \
+            or (user_move == 3 and computer_move == 1):
+        print(f'Computer wins with {possible_moves[(computer_move - 1)]} beating {possible_moves[(user_move - 1)]}\n\n')
+        return True
+    else:
+        print(f'User wins with {possible_moves[(user_move - 1)]} beating {possible_moves[(computer_move - 1)]}\n\n')
+        return False
+    # this was initially much longer, before I realized there were only 2 outcomes, I didn't need to if else every
+    # possibility
 
 
 main()
